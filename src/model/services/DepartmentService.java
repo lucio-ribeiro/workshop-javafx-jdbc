c3f004054ffff9a73e9cd4c7723b849a5eec5b03
lucio ribeiro <lucioribeiro2009@hotmail.com>
2024-06-05 18:39:15 -0300

    Salvando um novo departamento

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -10,9 +10,16 @@ public class DepartmentService {
 	
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 	
-	
 	public List<Department> findAll() {
 		return dao.findAll();
 	}
 
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
 }
